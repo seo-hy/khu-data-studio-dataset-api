1e1661643af08a0000a5661e757a6ec5ff23b85e
seoh02h <seok02h@khu.ac.kr>
2022-10-05 11:21:24 +0900

    Add api project

---
+++ src/main/java/com/seoh/khudatastudiomanageapi/KhuDataStudioManageApiApplication.java
@@ -0,0 +1,13 @@
+package com.seoh.khudatastudiomanageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhuDataStudioManageApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KhuDataStudioManageApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/seoh/khudatastudiomanageapi/KhuDataStudioManageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seoh.khudatastudiomanageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhuDataStudioManageApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
