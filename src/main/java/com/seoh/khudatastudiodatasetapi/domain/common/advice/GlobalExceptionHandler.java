7105f16b282a15908ac4d9e711513ec4b8308d56
seoh02h <seok02h@khu.ac.kr>
2022-11-22 00:18:57 +0900

    Add exception handler

--- src/main/java/com/seoh/khudatastudiodatasetapi/domain/common/advice/GlobalExceptionHandler.java
+++ src/main/java/com/seoh/khudatastudiodatasetapi/domain/common/advice/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.seoh.khudatastudiodatasetapi.domain.common.advice;
 
+import com.seoh.khudatastudiodatasetapi.domain.common.advice.exception.DatasetDataTypeNotValidException;
 import com.seoh.khudatastudiodatasetapi.domain.common.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,9 +13,25 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(Exception.class)
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-  public ErrorResponse.Message defaultException(Exception e) {
+  public ErrorResponse.Message defaultException(Exception ex) {
     return ErrorResponse.Message.builder()
-        .message(e.getMessage())
+        .message(ex.getMessage())
+        .build();
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ErrorResponse.Message methodArgumentNotValidException(MethodArgumentNotValidException ex){
+    return ErrorResponse.Message.builder()
+        .message(ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage())
+        .build();
+  }
+
+  @ExceptionHandler(DatasetDataTypeNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ErrorResponse.Message datasetDataTypeNotValidException(DatasetDataTypeNotValidException ex){
+    return ErrorResponse.Message.builder()
+        .message(ex.getMessage())
         .build();
   }
 
